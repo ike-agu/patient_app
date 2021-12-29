9a8b0fb0bfce969929e324df2312229c9a3eeb0d
Ikenna Agulobi <ikennaww@yahoo.fr>
2021-12-29 23:54:03 +0000

    added some state in the initialize method

--- patient.rb
+++ patient.rb
@@ -1,12 +1,18 @@
 class Patient
+  attr_reader :name, :cured
+
   def initialize(attributes = {})
     @name = attributes[:name]
-    @cured = attributes[:cured]
-    @blood_type = attributes[:blood_type]
+    @cured = attributes[:cured] || false
+    @blood_type = attributes[:blood_type] || "A"
+  end
+
+  def cure
+    @cured = true
   end
 end
 
-ikenna =  Patient.new({name: 'ikenna', cured: false})
-gozie = Patient.new({name: 'ikenna', cured: false, blood_type: 'A'})
+ikenna = Patient.new({ name: 'ikenna', cured: false })
+gozie = Patient.new({ name: 'Iyke strike', cured: false, blood_type: 'A' })
 p ikenna
 p gozie
