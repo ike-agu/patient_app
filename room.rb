27693b76231b8d1c9f454a09368a2d7289397fcd
Ikenna Agulobi <ikennaww@yahoo.fr>
2022-01-01 23:56:23 +0000

    Added Exceptional error message

--- room.rb
+++ room.rb
@@ -12,7 +12,7 @@ class Room
 
   def add_patient(patient)
     if full?
-
+      fail Exception, 'The room full'
     else
       @patients << patient
     patient.room = self
@@ -22,15 +22,15 @@ end
 
 room1 = Room.new(capacity: 2)
 puts 'Is the capacity of the rooms full?'
-# puts room1.full? ? 'Yes, it is full' : 'No! it is not'
+ puts room1.full? ? 'Yes, it is full' : 'No! it is not'
 
 Melanie = Patient.new(name: 'Melanie Ekani', age: 32)
 
 # puts "The patient is #{Melanie.name} and she is #{Melanie.age} years old"
 
 room1.add_patient(Melanie)
-p room1
-p Melanie.room
+# p room1
+puts Melanie.room
 
-Ikenna = Patient.new(name: 'Ikenna franklin', age: 34)
-room1.add_patient(Ikenna)
+# Ikenna = Patient.new(name: 'Ikenna franklin', age: 34)
+# room1.add_patient(Ikenna)
